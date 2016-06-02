18251517dab80722dc2a3600c8bdec018845105c
tobs9999 <t-fink@gmx.at>
2016-06-02 17:44:14 +0200

    Update serviceWorker.js

--- serviceworker/serviceWorker.js
+++ serviceworker/serviceWorker.js
@@ -76,7 +76,7 @@ this.addEventListener("message", function(event){
 				event.ports[0].postMessage({error: "entry not found"});
 			}
 			else {
-				event.ports[0].postMessage({resp: JSON.stringify(entry)});
+				event.ports[0].postMessage({resp: entry.clone()});
 			}
 			
 		})
