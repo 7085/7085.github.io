cc87a414528264db978aae48c6055aca63c80719
tobs9999 <t-fink@gmx.at>
2016-06-06 22:10:39 +0200

    Update serviceWorker.js

--- serviceworker/serviceWorker.js
+++ serviceworker/serviceWorker.js
@@ -59,13 +59,13 @@ this.addEventListener("fetch", function (event) {
 					h.append("x-forged", "true");
 					var r = new Response(buffer, {"status": 200, "statusText": "OK", headers: h});
 
-					console.log("forged: ", r);
+					console.log("forged: ", response);
 
 					/* cache it */
 					caches.open(event.request.url)
 						.then(function (cache) {
 							console.log("caching response...");
-							cache.put(event.request.url, r.clone());
+							cache.put(event.request.url, response.clone());
 						});
 
 					//return response.clone();
